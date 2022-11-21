7a92c6b08e4a0d0e1c3bd6c43b66c078a1f548b0
ShubhamIpangram <shubhamshukla@ipangram.com>
2022-11-21 19:24:04 +0530

    createRouters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-     <h1> This is INotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
